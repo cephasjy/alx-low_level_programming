750328450795c0ea96c5925882191e9e77f62270
cephasjy <cephasaddy55@gmail.com>
2023-06-16 19:39:38 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point.
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
